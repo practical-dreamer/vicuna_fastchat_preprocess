62536b2ddf49f2f8583f0924d34cd2733a84fd05
practicaldreamer <78515588+practicaldreamer@users.noreply.github.com>
2023-04-24 00:00:35 -0500

    added --strict_format flag
    Used to disable the various filters I deemed necessary when processing the ShareGPT dataset through the fastchat "make_supervised_data_module" function.

--- process_conversations.py
+++ process_conversations.py
@@ -5,14 +5,16 @@ from fastchat.train.train import make_supervised_data_module, DataArguments, pre
 from transformers import AutoTokenizer
 from tqdm import tqdm
 
-def roles_are_alternating(conversations, human_name, ai_name):
-    if not conversations or conversations[0]["from"] != human_name:
+def roles_are_alternating(conversations, human_name, ai_name, strict_format):
+    if strict_format and (not conversations or conversations[0]["from"] != human_name):
         return False
 
     for i in range(1, len(conversations)):
-        if conversations[i]["from"] == conversations[i - 1]["from"] or \
-           conversations[i]["from"] not in [human_name, ai_name] or \
-           conversations[i - 1]["from"] not in [human_name, ai_name]:
+        if strict_format and (
+            conversations[i]["from"] == conversations[i - 1]["from"] or \
+            conversations[i]["from"] not in [human_name, ai_name] or \
+            conversations[i - 1]["from"] not in [human_name, ai_name]
+        ):
             return False
     return True
 
@@ -34,7 +36,7 @@ def main(args):
         buffer = []
         for conversations in tqdm(conversations_generator, total=total_conversations, desc="Processing conversations"):
             # Filter out empty conversations, conversations with a "system" role, and conversations where roles aren't alternating
-            if conversations and roles_are_alternating(conversations, args.human_name, args.ai_name):
+            if conversations and roles_are_alternating(conversations, args.human_name, args.ai_name, args.strict_format):
                 buffer.append({"conversations": conversations})
 
             if len(buffer) >= 2:
@@ -65,6 +67,4 @@ if __name__ == '__main__':
     parser.add_argument('--input_json_path', required=True, help='Path to the input JSON file')
     parser.add_argument('--output_txt_path', required=True, help='Path to the output TXT file')
     parser.add_argument('--temp_json_path', default='buffer.json', help='Path to the temporary JSON file for buffering')
-
-    args = parser.parse_args()
-    main(args)
+    parser.add_argument('--strict_format', action='store_true', help
